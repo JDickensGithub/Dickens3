7fc48ca6d1de5b03f47cd75387c3254f76ae9c40
Collin Coil <112492870+CollinCoil@users.noreply.github.com>
2024-03-25 17:52:00 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,15 @@
-# J Dickens  James Dickens
+# C Coil  Collin Coil
 
 library(tidyverse)
+
+# The data source is the mpg data table
 head(mpg)
 summary(mpg$displ)
+
+# Producing a vertical boxplot for the variable displ
+ggplot(mapping = aes(y = mpg$displ)) + 
+  geom_boxplot(fill = "blue")
+
+# Altering the code for the boxplot so that it is produced horizontally (by switching x and y)
+ggplot(mapping = aes(x = mpg$displ)) + 
+  geom_boxplot(fill = "blue")
