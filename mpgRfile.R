74ffd47164f743b71416833988b31ed3e0d9fbf3
Rachel Rolle <155335768+rchlrolle@users.noreply.github.com>
2024-03-25 18:22:35 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,14 @@
-# J Dickens  James Dickens
+# Rachel Rolle
 
+#The data source is the mpg data table
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+#3
+mpg%>%
+ggplot(aes(y = cty))+geom_boxplot(fill = 'blue')
+
+#4
+mpg%>%
+ggplot(aes(x = cty))+geom_boxplot(fill = 'blue')
