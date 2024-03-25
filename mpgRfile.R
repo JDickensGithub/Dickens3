d5c4e15a94419c53d7b9425537e94363305a5832
S-DeFerrari <66847874+S-DeFerrari@users.noreply.github.com>
2024-03-25 18:21:09 -0400

    Update mpgRfile.R
    Adding my personal touches to this one :)

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,13 @@
-# J Dickens  James Dickens
+# S DeFerrari Stephen DeFerrari
+
+# The data source is the mpg data table
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+# vertical box plot
+boxplot(mpg$displ, col='Blue')
+
+# horizontal box plot
+boxplot(mpg$displ, col='Blue', horizontal = TRUE)
