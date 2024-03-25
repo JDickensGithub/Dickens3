490d1efcc565d97a37c7e93e6dab4b3fb6013f95
Miles Matthews <151972422+MilesMatthews@users.noreply.github.com>
2024-03-25 18:21:18 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,11 @@
-# J Dickens  James Dickens
+# Miles Matthews
+
+# The data source is the mpg table
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+ggplot(data = mpg, mapping = aes(y = displ))+
+geom_boxplot(fill = "blue")
+ggplot(data = mpg, mapping = aes(x = displ))+
+geom_boxplot(fill = "blue")
