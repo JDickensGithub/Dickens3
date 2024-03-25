3739f92fa53f4f5e7ac4ba1260cbaa331c4be4f2
Melanie Klein <122428535+melaniejklein@users.noreply.github.com>
2024-03-25 18:22:11 -0400

    Update mpgRfile.R
    updated the code

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,13 @@
-# J Dickens  James Dickens
-
+# M Klein  Melanie Klein
+# The data source is the mpg data table
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(mpg, aes(y = displ, fill = "blue")) +
+  geom_boxplot() +
+  scale_fill_manual(values = "blue")
+
+ggplot(mpg, aes(x = displ, fill = "blue")) +
+  geom_boxplot() +
+  scale_fill_manual(values = "blue")
