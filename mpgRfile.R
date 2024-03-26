be07e68c1ed51565a768d1af309b99e0b0bbd87a
ptlpavan <92137822+ptlpavan@users.noreply.github.com>
2024-03-26 18:33:47 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,13 @@
-# J Dickens  James Dickens
+# Pavan Patel
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x = drv, y = displ, color = "blue")) +
+  ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
+  xlab("transmission types") +
+  ylab("displacement") -> g1
+
+g1 + coord_flip()
