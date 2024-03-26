fd858e326c329acbb92624770dd9eab258024c7a
Bright <129008663+BrightAmenyo@users.noreply.github.com>
2024-03-26 18:14:07 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,20 @@
-# J Dickens  James Dickens
+# Bright Amenyo
 
 library(tidyverse)
+
+The data source is the mpg data table
 head(mpg)
 summary(mpg$displ)
+
+# Load necessary library
+library(ggplot2)
+
+
+# Create the boxplot
+ggplot(mpg, aes(x = "", y = displ, fill = "blue")) +
+  geom_boxplot() +
+  scale_fill_manual(values = "blue") +
+  theme_minimal() +
+  labs(x = NULL, y = "Displacement") +
+  ggtitle("Vertical Boxplot of Displacement")
+
