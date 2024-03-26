125a52896d683ee019093d7e3bb7a93f3c74e72c
Lisa Liubovich <143554458+lisaml02@users.noreply.github.com>
2024-03-26 18:18:53 -0400

    Update mpgRfile.R
    forking lab

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,6 +1,15 @@
-# J Dickens  James Dickens
+# L Liubovich ; Lisa Liubovich
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
-nrow(mpg)
+# the data source is the mgp data table
+
+ggplot(mpg, aes(x = displ, y = "", fill = factor(displ))) +
+  geom_boxplot() +
+  scale_fill_manual(values = "blue") +
+  labs(title = "Horizontal Boxplot of displ",
+       x = "displ",
+       y = "") +
+  theme_minimal() +
+  coord_flip()
