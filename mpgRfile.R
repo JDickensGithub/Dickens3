a9cf5f53d54d31987367648cee084f13e8594039
Sasha Uspenskaya <160512724+sashauspenskaya@users.noreply.github.com>
2024-03-26 17:41:14 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,12 @@
-# J Dickens  James Dickens
+# Sasha Uspenskaya
+# The data source is the mpg data table
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(y = displ, fill = "blue"))
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x = displ, fill = "blue"))
