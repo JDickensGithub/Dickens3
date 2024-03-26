b038f4ac0b0931b7e6d02ca8ac31c15c9d28b755
Carol <160648052+YCLCarol@users.noreply.github.com>
2024-03-26 18:11:05 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,8 @@
-# J Dickens  James Dickens
+#   Yen Chun Lin
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+The data source is the mpg data table
+ggplot(data=mpg) +geom_boxplot(mapping=aes(x=displ),fill="blue")
