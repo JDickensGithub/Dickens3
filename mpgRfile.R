dc89c806cb6d06613e35d14c725a5f68b4114c6c
Leigh Cummings <158106191+elcee2xx3@users.noreply.github.com>
2024-03-26 18:09:12 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,10 @@
-# J Dickens  James Dickens
+# L Cummings  Leigh Cummings
+# The data source is the mpg data table
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(data = mpg, aes(x= displ))+
+  geom_boxplot(aes(x=displ, fill="blue"))+
+  coord_flip()
