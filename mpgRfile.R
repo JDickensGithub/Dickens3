924cf134cea714e4ff8eabbb9b9af7f9017b12cc
Aileen McDonald <67488057+aileenmcd2@users.noreply.github.com>
2024-03-26 18:09:50 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,11 @@
-# J Dickens  James Dickens
+# A McDonald   Aileen McDonald
+
+# The data source is the mpg data table
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(mpg$displ, aes()) + 
+  geom_boxplot(color="blue") +
+  coord_flip()
