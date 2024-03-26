b5911923cad8a740704df5522ed9eb15d4c7668d
juliakato <122948087+juliakato@users.noreply.github.com>
2024-03-26 17:42:53 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,13 @@
-# J Dickens  James Dickens
+# J Kato  Julia Kato
+# The data source is the mpg data table
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(mpg, aes(x = displ, y = "", fill = factor(displ))) +
+  geom_boxplot(width = 0.5) +
+  scale_fill_manual(values = "blue") +
+  labs(x = "displ") +
+  theme_minimal() +
+  coord_flip()
