f5182e002170ee0a10903c8ec02ddc8481a49000
Longkai Liu <149982844+kai3315@users.noreply.github.com>
2024-03-28 10:54:07 +0800

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -3,3 +3,16 @@
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+mpg
+# My name is LongKai Liu
+# The data source is the mpg data table
+# This is my coding that generates Boxplots for transmission types
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv), fill = "blue") +
+  ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
+  xlab("transmission types") +
+  ylab("displacement") +
+  coord_flip()
